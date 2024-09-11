f1c6413e40f61b9826c37214a85de5ebc4dbda8a
Angelo Greco <angelo.greco2@studio.unibo.it>
2024-09-11 18:18:37 +0200

    final - changed index.js to properly handle frontend build

--- server/index.js
+++ server/index.js
@@ -1,44 +1,62 @@
-require('dotenv').config({ path: '../.env' }); // Load environment variables. Used for security
+const path = require('path');   // Handles file paths
+const _ = require("lodash");    // Utility library
+require('dotenv').config({path: path.resolve(__dirname+"/../.env")}); // Load environment variables. Used for security
+//console.log(require('dotenv').config());
 const express = require("express");
 const app = express();  //Express App
 const mongoose = require("mongoose");   // MongoDB interaction
-const path = require('path');   // Handles file paths
-const _ = require("lodash");    // Utility library
 const cors = require('cors');    // 'Cross-origin resource sharing' middleware
 const passport = require("passport"); // Authentication middleware
 const LocalStrategy = require("passport-local").Strategy; // Local authentication strategy for Passport
 const bcrypt = require("bcrypt"); // Password hashing library
 const session = require('express-session'); // Session middleware for Express
 const MongoDBStore = require('connect-mongodb-session')(session); // MongoDB session store
-const crypto = require('crypto'); // Cryptographic library for generating random strings
+//const crypto = require('crypto'); // Cryptographic library for generating random strings
+const helmet = require('helmet'); // Security middleware
 
 
 /* MIDDLEWARE */
 
 // Enable CORS for all routes, and allow credentials (cookies)
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5000', credentials: true }));
 
 // Add middleware for parsing JSON, urlencoded data and serving static files
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Use helmet to set 'CSP' header for security
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"], // Default policy
+      imgSrc: ["'self'", "https://site232431.tw.cs.unibo.it"],  // Policy for loading images and icons
+    },
+  },
+}));
 
 // Generate a secret key for the session (one-time only, currently stored in .env file)
 // const secretKey = crypto.randomBytes(64).toString('hex');
 // console.log(secretKey);
 
 // IMPOSTARE QUI I VALORI PER IL DATABASE DI TEST
-const mongoURIforTesting = '';
-const secretKeyforTesting = '';
+// const mongoURIforTesting = '';
+// const secretKeyforTesting = '';
 
-// Set the MongoDB URI
-const MONGODB_URI = process.env.MONGODB_URI || mongoURIforTesting;
+// Set the MongoDB URI        
+const MONGODB_URI = 'mongodb://${process.env.DB_USER}.${process.env.DB_PASS}@${process.env.DB_SITE}/${process.env.DB_NAME}?authSource=admin&writeConcern=majority'; // || mongoURIforTesting;
+//const MONGODB_URI = 'mongodb://site232431.ahB4ha7j@mongo_site232431/test?authSource=admin&writeConcern=majority';
+//console.log('MONGODB_URI: ', MONGODB_URI);
 
 // Create a new MongoDBStore instance for storing sessions in MongoDB
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'mySessions'
 });
+
+/* DATABASE CONNECTION */
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // Properties to use new node.js engines
+//const db = mongoose.connection;
 
 // Enable express-session, a session middleware for Express
 app.use(session({
@@ -79,11 +97,6 @@ passport.deserializeUser(async (id, done) => {
 const { ensureAuthenticated } = require('./middleware/customMiddlewares');
 
 
-/* DATABASE CONNECTION */
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // Properties to use new node.js engines
-//const db = mongoose.connection;
-
-
 
 /* IMPORT MODELS */
 const Event = require('./models/Event');
@@ -106,16 +119,13 @@ app.use('/api/notes', ensureAuthenticated, notesRoutes);
 
 
 /* ALL ROUTES */  // Mostly debugs, to be removed
-
 // app.get('/api/test', (req, res) => {   // DEBUG
 //   res.json({ user: req.user });
 // });
-
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname+'/../client/public/index.html'));
 // });
-
 
 
 
